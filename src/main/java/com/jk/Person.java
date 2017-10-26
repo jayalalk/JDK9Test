ed5bc12a0f9b573681a6183e902a1f2e38607be2
jayalalk <jayalalk@gmail.com>
2017-10-26 19:03:23 +0530

    added

---
+++ src/main/java/com/jk/Person.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+public class Person{
+	
+	String name=null;
+	String other=null;
+	
+	public Person(String name, String other){
+		this.name=name;
+		this.other=other;
+		//return;
+	}
+
+}
