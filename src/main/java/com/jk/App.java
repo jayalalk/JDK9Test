00248ead9f1be1e2765603f990e8e871bff3604e
jayalalk <jayalalk@gmail.com>
2017-10-26 18:51:29 +0530

    addtes testload

--- src/main/java/com/jk/App.java
+++ src/main/java/com/jk/App.java
@@ -1,13 +1,33 @@
 package com.jk;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+	
+
+public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        testLoad();
+        
+    }
+    
+    
+    public static void testLoad(){
+    	
+    	List<Person> list = new ArrayList<Person>();
+        for (int i = 0; i <= 100000; i++) {
+            list.add(new Person("Jim", "Knopf"));
+        }
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory is bytes: " + memory);
+   //     System.out.println("Used memory is megabytes: "                 + bytesToMegabytes(memory));
+    	
     }
 }
